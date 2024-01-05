be03ee46765b7ecc59fef77e7d4b2f55b8519fb1
extreme4all <40169115+extreme4all@users.noreply.github.com>
2024-01-05 22:07:47 +0100

    create the staging report table

--- mysql/docker-entrypoint-initdb.d/01_tables.sql
+++ mysql/docker-entrypoint-initdb.d/01_tables.sql
@@ -15,3 +15,32 @@ CREATE TABLE Players (
     ultimate_ironman BOOLEAN,
     normalized_name TEXT
 );
+
+-- Insert into
+CREATE TABLE `stgReports` (
+  `ID` bigint NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reportedID` int NOT NULL,
+  `reportingID` int NOT NULL,
+  `region_id` int NOT NULL,
+  `x_coord` int NOT NULL,
+  `y_coord` int NOT NULL,
+  `z_coord` int NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `manual_detect` tinyint(1) DEFAULT NULL,
+  `on_members_world` int DEFAULT NULL,
+  `on_pvp_world` tinyint DEFAULT NULL,
+  `world_number` int DEFAULT NULL,
+  `equip_head_id` int DEFAULT NULL,
+  `equip_amulet_id` int DEFAULT NULL,
+  `equip_torso_id` int DEFAULT NULL,
+  `equip_legs_id` int DEFAULT NULL,
+  `equip_boots_id` int DEFAULT NULL,
+  `equip_cape_id` int DEFAULT NULL,
+  `equip_hands_id` int DEFAULT NULL,
+  `equip_weapon_id` int DEFAULT NULL,
+  `equip_shield_id` int DEFAULT NULL,
+  `equip_ge_value` bigint DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+)
+;
