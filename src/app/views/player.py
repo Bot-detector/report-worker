891e6ea80ec49e807bdfcbb47376a59c1562ad1a
extreme4all <>
2024-05-23 01:08:42 +0200

    BUG FIX

--- src/app/views/player.py
+++ src/app/views/player.py
@@ -6,11 +6,11 @@ from pydantic import BaseModel
 
 class PlayerCreate(BaseModel):
     name: str
-    possible_ban: Optional[bool] = None
-    confirmed_ban: Optional[bool] = None
-    confirmed_player: Optional[bool] = None
-    label_id: Optional[int] = None
-    label_jagex: Optional[int] = None
+    possible_ban: Optional[bool] = 0
+    confirmed_ban: Optional[bool] = 0
+    confirmed_player: Optional[bool] = 0
+    label_id: Optional[int] = 0
+    label_jagex: Optional[int] = 0
     ironman: Optional[bool] = None
     hardcore_ironman: Optional[bool] = None
     ultimate_ironman: Optional[bool] = None
